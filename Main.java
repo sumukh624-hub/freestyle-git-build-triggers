7fdb1541d111a1cc269b7ecca9ed97a155f73d3d
sumukh624-hub <sumukh624@gmail.com>
2026-01-30 09:33:19 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-        double side = 6.0;
+        double side = 7.0;
         double area = side * side;
         System.out.println("The side length is: " + side);
         System.out.println("The area of the square is: " + area);
